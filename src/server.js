d041af96abca26cc6caba65cb6a3136c056da53e
AndreLudwig <andre@roadseg.com.br>
2020-11-10 23:38:55 -0300

    Segunda parte aula 4, add express, nodemon, criando servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log(24+2)
+// importar dependencia
+const express = require('express');
+const path = require('path');
+
+// iniciando o express
+const server = express()
+server
+    // utilizando os arquivos estáticos
+    .use(express.static('public'))
+    // criar uma rota
+    .get('/', (request, response) => {
+        return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+    })
+
+// ligar o servidor
+server.listen(5500)
